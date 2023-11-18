4f88362e80be9510108953a82ad92aaf57db580f
Michael Spiegel <michaelspiegel@live.at>
2023-11-18 15:25:33 +0100

    Readded clear button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -150,22 +150,24 @@
 			<label for="lineWidth">Line Width</label>
 			<input id="lineWidth" name="lineWidth" type="number" min="1" bind:value={lineWidth}>
 
-	<div class="h-screen w-screen bg-slate-700 justify-center flex">
-		<div class="flex flex-col">
-			<div class="bg-black p-8 grow-0 touch-none" on:mousedown={onMouseDown} on:touchdown={onTouch}>
-				<div class="flex">
-					{#each {length: MATRIX_SIZE_X} as _, x }
-						<div class="flex-col"> 
-							{#each {length: MATRIX_SIZE_Y} as _, y }
-								<div class={"h-2 w-2 ml-1 mb-1 rounded-full mxPixel " + colorForCode($matrixStore[x][y])} data-posX={x}, data-posY={y}></div>
-							{/each}
-						</div>
-					{/each}
+			<button id="clear" on:click={onCancelClicked}>Clear</button>
+		</div>
+		<div class="h-screen w-screen bg-slate-700 justify-center flex">
+			<div class="flex flex-col">
+				<div class="bg-black p-8 grow-0 touch-none" on:mousedown={onMouseDown} on:touchdown={onTouch}>
+					<div class="flex">
+						{#each {length: MATRIX_SIZE_X} as _, x }
+							<div class="flex-col">
+								{#each {length: MATRIX_SIZE_Y} as _, y }
+									<div class={"h-2 w-2 ml-1 mb-1 rounded-full mxPixel " + colorForCode($matrixStore[x][y])} data-posX={x}, data-posY={y}></div>
+								{/each}
+							</div>
+						{/each}
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
 	<span class="dot" id="cursor" bind:this={cursor} />
 </main>
 
